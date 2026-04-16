0220b67af5880503bd0df2563ee0c5de80189fbf
Anh Ha <anhha459401@gmail.com>
2026-04-16 10:23:24 +0700

    fix accept friend

--- backend/src/models/FriendRequest.js
+++ backend/src/models/FriendRequest.js
@@ -2,24 +2,16 @@ import mongoose from "mongoose";
 
 const friendRequestSchema = new mongoose.Schema(
   {
-    from: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    to: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    message: {
+    from: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    to: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    message: { type: String, maxlength: 300 },
+    status: {
       type: String,
-      maxlength: 300,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true },
 );
 
 friendRequestSchema.index({ from: 1, to: 1 }, { unique: true });
